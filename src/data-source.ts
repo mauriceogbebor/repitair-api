2adb481afcb35843018a88fb70bf338fb33bff7d
Maurice Ogbebor <mauriceogbebor@gmail.com>
2026-04-19 20:53:35 +0100

    feat: added template entity to typeorm data source

--- src/data-source.ts
+++ src/data-source.ts
@@ -2,7 +2,7 @@ import "reflect-metadata";
 import { DataSource } from "typeorm";
 import { config as dotenvConfig } from "dotenv";
 
-import { User, Repit, PushToken } from "./entities";
+import { User, Repit, PushToken, Template } from "./entities";
 
 dotenvConfig();
 
@@ -20,7 +20,7 @@ const isCompiled = __filename.endsWith(".js");
 export default new DataSource({
   type: "postgres",
   url: process.env.DATABASE_URL || "postgresql://repitair:repitair@localhost:5432/repitair",
-  entities: [User, Repit, PushToken],
+  entities: [User, Repit, PushToken, Template],
   migrations: [isCompiled ? "dist/migrations/*.js" : "src/migrations/*.ts"],
   synchronize: false,
   logging: true,
